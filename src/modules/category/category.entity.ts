cd5b69ba5872e5225c8690898d5f53cb57050ee8
Lucas <lucasab948@gmail.com>
2026-08-23 14:04:12 -0300

    modification: products in categories entity modified

--- src/modules/category/category.entity.ts
+++ src/modules/category/category.entity.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from "typeorm";
-import { ProductEntity } from "../product/product.entity";
+import { Product } from "../product/product.entity";
 
 @Entity("category")
 export class Category {
@@ -15,6 +15,6 @@ export class Category {
   @Column({ name: "description", type: "text", nullable: true })
   description?: string;
 
-  @OneToMany(() => ProductEntity, (product) => product.category)
-  products!: ProductEntity[];
+  @OneToMany(() => Product, (product) => product.categories)
+  products!: Product[];
 }
